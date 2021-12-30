8d7895a2891eb7ac212b4fb82829cc7c1cd79542
ChanakaDushmantha <2016sp314@gmail.com>
2021-12-30 10:33:43 +0530

    AWS config and detectLabels

--- routes/vision.js
+++ routes/vision.js
@@ -1,16 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var AWS = require("aws-sdk"); // Load the SDK
+var dotenv = require("dotenv");
 
-router.post('/classify', function(req, res, next) {
-  // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
+router.post("/classify", function (req, res, next) {
 
-  // Your code starts here //
+  dotenv.config(); //loads environment variables
 
-  // Your code ends here //
+  const config = new AWS.Config({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+  });
 
-  res.json({
-    "labels": response
+  const client = new AWS.Rekognition();
+
+  //console.log(req.files);
+
+  const params = {
+    Image: {
+      Bytes: req.files.file.data, //uploaded file
+    },
+  };
+
+  client.detectLabels(params, function (err, response) {
+    if (err) {
+      console.log(err, err.stack); // if an error occurred from AWS
+      res.status(500).json({
+        error: err.code,
+      });
+    } else {
+      res.status(200).json({
+        labels: response.Labels.map((label) => label.Name),
+      });
+    }
   });
 });
 
